31929e3f2d7170feb7089d6a669318291fe97d27
dharmik <dharmikd@zignuts.com>
2023-02-24 10:16:05 +0530

    init commit

--- relationship-app/app/Http/Controllers/PhotoController.php
+++ relationship-app/app/Http/Controllers/PhotoController.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Request;
 class PhotoController extends Controller
 {
     public function showPhoto($id){
-        $photo = Photo::find($id);
-
-        foreach ($photo->reviews as $review) {
-            return $review;
-        }
-        return $photo;
+       
     }
 }
